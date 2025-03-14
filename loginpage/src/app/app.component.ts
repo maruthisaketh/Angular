86fa6d43ead65deacc5aacb33d92054df550e95b
maruthi-16 <maruthi.abbaraju@zolontech.com>
2025-03-14 15:09:38 -0400

    Start Routing

--- loginpage/src/app/app.component.ts
+++ loginpage/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, LogoutComponent],
+  imports: [RouterOutlet, LoginComponent, LogoutComponent, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- loginpage/src/app/app.routes.ts
+++ loginpage/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Login', component: LoginComponent },
+    { path: 'Logout', component: LogoutComponent },
+];
